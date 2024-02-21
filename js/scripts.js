86cadbc46ba2483b23d25337f50c9f8ee19876ec
stefleon33 <stefleon33@gmail.com>
2024-02-21 10:54:45 -0500

    Added starter pokemon to variable pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let pokemonList = [
+    {name: 'Bulbasaur', height: 2, type: ['grass', 'poison'], pokedex: '0001'}, 
+    {name: 'Ivysaur', height: 3, type:['grass', 'poison'], pokedex: '0002'},
+    {name: 'Venusaur', height: 6, type:['grass', 'poison'], pokedex: '0003'},
+    {name: 'Charmander', height: 2, type: 'fire', pokedex: '0004'}, 
+    {name: 'Charmeleon', height: 3, type: 'fire', pokedex: '0005'},
+    {name: 'Charizard', height: 5, type: 'fire', pokedex: '0006'},
+    {name: 'Squirtle', height: 1, type: 'water', pokedex: '0007'},
+    {name: 'Wartortle', height: 3, type: 'water', pokedex: '0008'},
+    {name: 'Blastoise', height: 5, type: 'water', pokedex: '0009'},
+]
