23d52e1a488662130d341f352ed0f017aeee9e68
stefleon33 <stefleon33@gmail.com>
2024-03-06 17:14:03 -0500

    Add loadList and loadDetails functions

--- js/scripts.js
+++ js/scripts.js
@@ -48,6 +48,37 @@ let pokemonRepository = (function() {
         })
     }
 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+          return response.json();
+        }).then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+            console.log(pokemon);
+          });
+        }).catch(function (e) {
+          console.error(e);
+        })
+      }
+      
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then(function (details) {
+          // Now we add the details to the item
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e) {
+          console.error(e);
+        });
+      }
+
     function showDetails(pokemon) {
         console.log(pokemon);
     }
