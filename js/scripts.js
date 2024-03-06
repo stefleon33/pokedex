84266b1c2a2af621e086e13f4aa825c3db94dea0
stefleon33 <stefleon33@gmail.com>
2024-03-06 17:13:43 -0500

    Updated event listener to show details

--- js/scripts.js
+++ js/scripts.js
@@ -43,8 +43,8 @@ let pokemonRepository = (function() {
         button.classList.add("button-class")
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
-        button.addEventListener('click', function (_showDetails){
-            console.log(pokemon);
+        button.addEventListener("click", function (_event) {
+            showDetails(pokemon);
         })
     }
 
