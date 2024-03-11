7a292303e8ea097800fa85f443a33201a8bf4e2e
stefleon33 <stefleon33@gmail.com>
2024-03-11 13:20:23 -0400

    modal js

--- js/scripts.js
+++ js/scripts.js
@@ -15,17 +15,83 @@ let pokemonRepository = (function () {
         document.write('<br>');
     }
 
-    function add(pokemon){
-        if (
-            typeof pokemon === "object" &&
-            "name" in pokemon &&
-            "height" in pokemon &&
-            "types" in pokemon
-        ) {
-            repository.push(pokemon);
-        } else {
-            console.log("pokemon is not correct");
+    let modalContainer = document.querySelector('#modal-container');
+
+    // Function to show deatils of a Pokemon in a modal
+    function showModal(pokemon) {
+      
+    //Clear all exisiting modal content
+      modalContainer.innerHTML = '';
+
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+
+      //Close button details
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = title
+
+      let contentElement = document.createElement('p');
+      contentElement.innerText = text;
+
+      
+     /*  let modalTitle = document.createElement('h1');
+      let modalBody = document.createElement('p');
+      modalTitle.empty();
+      modalBody.empty(); */
+        
+
+       //Creating elements to display Pokemon details in the modal
+       let nameElement = ('<h1>' + pokemon.name + '</h1>');
+       let imageElement = (
+           '<img class="modal-img" style="width:50%>').attr(
+               "src",pokemon.imageUrl);
+       let heightElement = ("<p>" + "Height: " + pokemon.height + "</p>");
+       let weightElement = ("<p>" + "Weight: " + pokemon.weight + "</p>");
+       let typesElement = ("<p>" + "Types: " + pokemon.types.join(", ") + "</p>");
+       let abilitiesElement = ("<p>" + "Abilities: " + pokemon.abilities.join(", ") + "</p>");
+
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modalContainer.appendChild(modal);
+      //Appending elements to the modal
+      titleElement.appendChild(nameElement);
+      contentElement.appendChild(imageElement);
+      contentElement.appendChild(heightElement);
+      contentElement.appendChild(weightElement);
+      contentElement.appendChild(typesElement);
+      contentElement.appendChild(abilitiesElement);
+      
+
+      modalContainer.classList.add('is-visible');
+
+      function hideModal(){
+        modalContainer.classList.remove('is-visible');
+      }
+
+      window.addEventListener('keydown', (e) =>{
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+          hideModal();
         }
+      });
+
+      modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+          hideModal();
+        }
+      })
+
+      document.querySelector('.pokemon-list').addEventListener('click', () => {
+        showModal('')
+      });
+    }
+        
     }
 
     function getAll() {
@@ -81,9 +147,10 @@ let pokemonRepository = (function () {
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function() {
-            console.log(pokemon);
+            showModal (pokemon);
         });
-    }
+        }
+    
     
     return {
         add: add,
@@ -101,5 +168,14 @@ pokemonRepository.loadList().then(function() {
     // Now the data is loaded!
     pokemonRepository.getAll().forEach(function(pokemon){
       pokemonRepository.addListItem(pokemon);
+
+      let listItem = document.createElement("li");
+      listItem.classList.add("list-group-item");
+      listItem.innerText = pokemon.name;
+
+      listItem.addEventListener("click", () =>{
+        pokemonRepository.showDetails(pokemon);
+        $("#modal-container").modal("show");
+      });
     });
   });
