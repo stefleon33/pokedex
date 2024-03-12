6bfcdeaa57a9d1c219cb8063b10a6ebff14baca6
stefleon33 <stefleon33@gmail.com>
2024-03-12 13:03:34 -0400

    Added weight details

--- js/scripts.js
+++ js/scripts.js
@@ -149,6 +149,7 @@ let pokemonRepository = (function () {
           // Now we add the details to the item
           item.imageUrl = details.sprites.front_default;
           item.height = details.height;
+          item.weight = details.weight;
           item.types = details.types;
           item.abilities = details.abilities;
         }).catch(function (e) {
